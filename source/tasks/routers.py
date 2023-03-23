0d74f326f91f8072c857dcc59f12f72439603ab0
Гут Владимир <vladimir.monolith@yandex.ru>
2023-03-23 20:53:27 +0300

    Доработано использование Celery

--- source/tasks/routers.py
+++ source/tasks/routers.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, BackgroundTasks, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 from http import HTTPStatus
+
+from fastapi import APIRouter, Depends
+
 from authentication.manager import current_active_user
-from database.connection import get_async_session
 from database.models import User
 
 from .tasks import send_email_uploads_report
@@ -15,15 +15,8 @@ router = APIRouter(
 
 @router.get('/uploads_report')
 def get_uploads_report(user: User = Depends(current_active_user)):
-    # 1400 ms - Клиент ждет
-    # send_email_uploads_report(user=user, session=session)
-    # 500 ms - Задача выполняется на фоне FastAPI в event loop'е или в другом треде
-    # background_tasks.add_task(
-    #     send_email_uploads_report,
-    #     user=user
-    # )
-    # 600 ms - Задача выполняется воркером Celery в отдельном процессе
-    send_email_uploads_report.delay(username=user.username)
+    """Отправляем отчёт о загрузках пользователя с помощью Celery."""
+    send_email_uploads_report.delay(user.username)
     return {
         'status': HTTPStatus.OK,
         'data': 'Письмо отправлено',
